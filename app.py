4d9da2b28afb7be99016f3ff8c01f846a38d9bd4
svasiliev <svasiliev@change.com>
2022-06-16 10:58:02 +0300

    мин каркас ( обработка мыши )

--- app.py
+++ app.py
@@ -1,12 +1,36 @@
-import json as json_parser
+from random import random
+from kivy.app import App
+from kivy.uix.button import Button
+from kivy.uix.widget import Widget
+from kivy.graphics import Color, Ellipse, Line
+
+class MyDashboardWidget(Widget):
+
+    def on_touch_move(self, touch):
+        print("on_touch_move")
+
+    def on_touch_move(self,touch):
+        print("on_touch_move")
+
+    def on_touch_up(self,touch):
+        print("on_touch_up")
 
 
-with open('./example/data/demo.json') as read_file:
-    superDemoExample = json_parser.load(read_file)
+class MyApp(App):
+
+    def build(self):
+        parent = Widget()
+        self.painter = MyDashboardWidget()
+
+        clearbtn = Button(text='Clear')
+        clearbtn.bind(on_release=self.clear_canvas)
+        parent.add_widget(self.painter)
+        parent.add_widget(clearbtn)
+        return parent
+
+    def clear_canvas(self, obj):
+        self.painter.canvas.clear()
 
 
-json_string = json_parser.dumps(superDemoExample)
-
-print ( type( superDemoExample ) )
-print ("DUMP JSON" )
-print ( json_string )
+if __name__ == '__main__':
+    MyApp().run()
