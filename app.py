7984d23cdf54b533268e32c3e5ced558c93ab787
svasiliev <svasiliev@change.com>
2022-06-16 23:28:09 +0300

    add parse mqqt message

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 import sys
 import time
 import threading
+import json as json_parser
 import paho.mqtt.client as mqtt
+from random import random
 
 from kivy.app import App
 from kivy.lang import Builder
@@ -14,8 +16,6 @@ from kivy.uix.behaviors import DragBehavior
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.button import Button
 from kivy.uix.widget import Widget
-
-from random import random
  
 import MyDashboardWidget
 
@@ -28,44 +28,48 @@ class RootWidget(BoxLayout):
     stop    = threading.Event()
     client  = mqtt.Client("client-001")
 
+    errors = [];
+    beacons = {};
+    stations = [];
+
+
     @mainthread
     def connect_to_broker(self):
         if self.stop.is_set() == False:
-            threading.Thread(target=self.infinite_loop).start()
+            threading.Thread(target=self.thread_mqtt_loop).start()
 
-    def on_mqtt_connect(_client, userdata, flags, rc, t1):
-        print("Connected with result code "+str(rc))
-        _client.client.subscribe('beacons/office')
-    
-    def on_mqtt_message():
-        print("received message =",str(message.payload.decode("utf-8")))
-
-
-    def infinite_loop(self):
+    def thread_mqtt_loop(self):
         connack_rc = -1
         try:
             self.client.on_connect=self.on_mqtt_connect
             self.client.on_message=self.on_mqtt_message
             self.client.connect('192.168.4.1')
-            
+
             self.client.loop_start()
             while connack_rc == -1:
-                if self.stop.is_set():
+                if self.stop.is_set(): # флаг на выход
                     return
                 time.sleep(0.5)
             print("CONNACK reason code: %d" % (connack_rc))
+            self.client.loop_stop()
             
         except:
             print("Oops!", sys.exc_info()[0], "occurred.")
 
-        #iteration = 0
-        #while True:
-        #    if self.stop.is_set():
-        #        # Stop running this thread so the main Python process can exit.
-        #        return
-        #    iteration += 1
-        #    print('Infinite loop, iteration {}.'.format(iteration))
-        #    time.sleep(1)
+    def on_mqtt_connect(self, userdata, flags, rc, t1):
+        print("Connected with result code "+str(rc))
+        self.client.subscribe('/beacons/office')
+    
+    def on_mqtt_message(self, userdata, rc1, message ):
+        json_str = str(message.payload.decode("utf-8"))
+        json_obj = None
+        print("received message =",json_str)
+        json_obj = json_parser.loads( json_str )
+        #
+        for i in range(len(json_obj['e'])):
+            mac     = json_obj['e'][i]['m'];
+            station = json_obj['st'];
+            print(mac)
 
 class Application(App):
 
